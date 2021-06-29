48dec5b73ebb6a90bce0592298bc4a0229e0ae9f
srec0043 <srec0043@SSPN-SREC004301.KoreaZinc.co.kr>
2021-06-29 14:54:33 +0900

    최초 셋팅 커밋

---
+++ src/test/java/com/ssb/eureka/EurekaFoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssb.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaFoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
